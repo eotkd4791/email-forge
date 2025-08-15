5df521669e74c35557982a0125ed50c5dd2bc600
eotkd4791 <eotkd4791@gmail.com>
2025-08-15 19:17:29 +0900

    remove: unused service

--- src/mail-sender/mail-sender.module.ts
+++ src/mail-sender/mail-sender.module.ts
@@ -1,28 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AWSSESService } from './providers/aws-ses/aws-ses.service';
 import { ResendService } from './providers/resend/resend.service';
-import { SendGridService } from './providers/sendgrid/sendgrid.service';
 
 @Module({
   providers: [
     AWSSESService,
     ResendService,
-    SendGridService,
     {
       provide: 'MailSender',
-      useFactory: (awsSesService: AWSSESService, resendService: ResendService, sendGridService: SendGridService) => {
+      useFactory: (awsSesService: AWSSESService, resendService: ResendService) => {
         switch (process.env.EMAIL_PROVIDER) {
           case 'AWS_SES':
             return awsSesService;
           case 'RESEND':
             return resendService;
-          case 'SENDGRID':
-            return sendGridService;
           default:
             return awsSesService;
         }
       },
-      inject: [AWSSESService, ResendService, SendGridService],
+      inject: [AWSSESService, ResendService],
     },
   ],
   exports: ['MailSender'],
