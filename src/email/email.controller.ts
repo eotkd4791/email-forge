e8027d1cd0429d7d1e8f7babd0289d18f4e566e3
eotkd4791 <eotkd4791@gmail.com>
2025-08-16 17:28:53 +0900

    fix: update http status code

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { EmailService } from './email.service';
 
 @Controller('email')
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
+  @HttpCode(HttpStatus.ACCEPTED)
   @Post('send')
   async sendEmail(@Body() { to, name, subject }: { to: string; name: string; subject: string }) {
     await this.emailService.enqueueWelcomeEmail(to, name, subject);
