ce27ead08cd8c60a9ea692c37e48a6d1efd9cd88
eotkd4791 <eotkd4791@gmail.com>
2025-08-16 18:16:24 +0900

    feat: Enhance MailSender module with ConfigService and SendGrid support

--- src/mail-sender/mail-sender.module.ts
+++ src/mail-sender/mail-sender.module.ts
@@ -1,24 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { type Config } from '../config/config.schema';
 import { AWSSESService } from './providers/aws-ses/aws-ses.service';
 import { ResendService } from './providers/resend/resend.service';
+import { SendGridService } from './providers/sendgrid/sendgrid.service';
 
 @Module({
   providers: [
     AWSSESService,
     ResendService,
+    SendGridService,
     {
       provide: 'MailSender',
-      useFactory: (awsSesService: AWSSESService, resendService: ResendService) => {
-        switch (process.env.EMAIL_PROVIDER) {
+      useFactory: (
+        configService: ConfigService<Config>,
+        awsSesService: AWSSESService,
+        resendService: ResendService,
+        sendGridService: SendGridService,
+      ) => {
+        const provider = configService.get('EMAIL_PROVIDER', { infer: true });
+        switch (provider) {
           case 'AWS_SES':
             return awsSesService;
           case 'RESEND':
             return resendService;
+          case 'SENDGRID':
+            return sendGridService;
           default:
+            // 기본값으로 SES를 사용하거나, 에러를 던질 수 있습니다.
             return awsSesService;
         }
       },
-      inject: [AWSSESService, ResendService],
+      inject: [ConfigService, AWSSESService, ResendService, SendGridService],
     },
   ],
   exports: ['MailSender'],
